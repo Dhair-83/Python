ab668bcf1a0c5e6b8237346d12d95d6dfd4bd054
Dhair-83 <dhairyal0803@gmail.com>
2025-02-05 21:57:17 +0530

    Comparison

---
+++ Python_Operators.py
@@ -0,0 +1,19 @@
+t8=1001
+t3=1001
+
+print("Add-",t8+t3)
+print("Subtract-",t8-t3)
+print("Multiply-",t8*t3)
+print("Division-",t3/t8)
+print("Subtract-",t3-t8)
+print("Division-",t8/t3)
+print("Floor Division-",t3//t8)
+print("Exponential-",t8**t3)
+print("Modulus-",t3%t8)
+
+print(t8==t3)
+print(t8<=t3)
+print(t8>=t3)
+print(t8!=t3)
+print(t8<t3)
+print(t8>t3)
