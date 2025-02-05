cc6fbd530f0ca13d22975f8efe221476967adad9
Dhair-83 <dhairyal0803@gmail.com>
2025-02-05 21:38:50 +0530

    Date Types

---
+++ Pro2.py
@@ -0,0 +1,18 @@
+name="Dhairya"
+school="Narayana"
+grade=10
+status=True
+weight=float(56)
+
+print("My name is ",name)
+print("I study in ",school," in class ",grade)
+print("My weight is",weight)
+
+weight=int(56.95)
+print(weight)
+
+height=str(172)
+print(height)
+
+name=int("Dhairya")
+print(name)
