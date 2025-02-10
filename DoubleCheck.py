764eb42ec6e9c6ea568f9ed47b1a33daf768f530
Dhair-83 <dhairyal0803@gmail.com>
2025-02-10 21:47:05 +0530

    Checkkk

---
+++ DoubleCheck.py
@@ -0,0 +1,10 @@
+number=int(input("Enter Number here:"))
+
+if number>35:
+    print("The numeber is greater than 35!")
+    if number%2==0:
+        print("Even number!")
+    else:
+        print("Odd number!")
+else:
+    print("Number is less than 35!")
