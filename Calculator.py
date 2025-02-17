6317d38b47b14ca8cd33e6cadc039707640aa80f
Dhair-83 <dhairyal0803@gmail.com>
2025-02-17 21:57:59 +0530

    CALCCC

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+def add(l,k):
+    return l+k
+def sub(l,k):
+        return l-k
+def mul(l,k):
+      return l*k
+def div(l,k):
+      return l/k
+
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+print("Addition-",add(num1,num2))
+print("Subtraction-",sub(num1,num2))
+print("Multiplication-",mul(num1,num2))
+print("Division-",div(num1,num2))
