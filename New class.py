9bc408dc910f4ef5e18f0935dbfbb7942f17ec62
Dhair-83 <dhairyal0803@gmail.com>
2025-03-03 21:30:34 +0530

    INTRO!

---
+++ New class.py
@@ -0,0 +1,14 @@
+class student:
+    name="Dhairya"
+    grade=10
+
+    def intro(self):
+        print("Hi! I study in Narayana.")
+
+    def myself(self):
+        print("My name is",self.name)
+        print("I study in class",self.grade)
+
+amz=student()
+amz.intro()
+amz.myself()
